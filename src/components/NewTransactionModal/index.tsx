2d7f3343d137a3b760b9c5f041f2161bdbd3f445
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-06-21 12:07:37 -0300

    feat: inserção e aplicação funcional

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -30,15 +30,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     setType('withdraw')
   }
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type,
     })
+
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose();
   }
 
   return (
